5ba8548a82e77aab51729eed7f65c8f3bd2ed684
Dennis Goldin <dennis.goldin@gmail.com>
2016-02-11 00:25:47 -0500

    Had mode works. Add comments. Add CSS override for FontAwesome Icons.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@ var text1 = "Jelly sweet roll drage gummies. Jelly-o cotton candy gingerbread. D
 var text2 = "Cotton candy chocolate cake macaroon applicake I love. Tart I love powder. I love tiramisu lollipop pie. Pudding gummi bears sweet. Candy chocolate I love jujubes pie donut. Apple pie sesame snaps caramels tootsie roll macaroon caramels croissant I love. Sweet roll chocolate lemon drops. I love I love muffin gummi bears I love cotton candy.";
 var text3 = "Lemon drops ice cream I love danish cake jelly cupcake. Bear claw I love I love tart I love liquorice bear claw. Halvah chocolate gummi bears jelly-o jelly-o lollipop lemon drops liquorice. Candy canes tiramisu candy faworki apple pie. Chocolate jujubes sesame snaps ice cream cupcake. Halvah topping chupa chups biscuit. I love I love chupa chups tootsie roll marshmallow tart gummi bears chocolate.";
 
-var storageState1 = "closed";
+var state1 = "closed";
 var content1 = document.querySelector("#content1");
 var event1 = document.querySelector("#hoverBox1");
 var arrow1 = document.querySelector("#arrow1");
@@ -11,20 +11,30 @@ var hoverBox1 = document.querySelector("#hoverBox1");
 event1.addEventListener('click', toggle1);
 
 function toggle1() {
-  if (storageState1 === "closed") {
-      content1.textContent = text1;
-      arrow1.className = "fa fa-arrow-down arrowIcon";
-      hoverBox1.className = "hoverDownArrow";
-      storageState1 = "open";
+  if (state1 === "closed") {
+    content1.textContent = text1;
+    arrow1.className = "fa fa-arrow-down arrowIcon fa-1x";
+    hoverBox1.className = "hoverDownArrow";
+    state1 = "open";
+    // close #2
+    content2.textContent = "";
+    state2 = "closed";
+    hoverBox2.className = "hoverRightArrow";
+    arrow2.className = "fa fa-arrow-right arrowIcon fa-1x";
+    // close #3
+    content3.textContent = "";
+    state3 = "closed";
+    hoverBox3.className = "hoverRightArrow";
+    arrow3.className = "fa fa-arrow-right arrowIcon fa-1x";
   } else {
     content1.textContent = "";
-    storageState1 = "closed";
+    state1 = "closed";
     hoverBox1.className = "hoverRightArrow";
-    arrow1.className = "fa fa-arrow-right arrowIcon";
+    arrow1.className = "fa fa-arrow-right arrowIcon fa-1x";
   }
 }
 
-var storageState2 = "closed";
+var state2 = "closed";
 var content2 = document.querySelector("#content2");
 var event2 = document.querySelector("#hoverBox2");
 var arrow2 = document.querySelector("#arrow2");
@@ -33,20 +43,30 @@ var hoverBox2 = document.querySelector("#hoverBox2");
 event2.addEventListener('click', toggle2);
 
 function toggle2() {
-  if (storageState2 === "closed") {
+  if (state2 === "closed") {
       content2.textContent = text1;
-      arrow2.className = "fa fa-arrow-down arrowIcon";
+      arrow2.className = "fa fa-arrow-down arrowIcon fa-1x";
       hoverBox2.className = "hoverDownArrow";
-      storageState2 = "open";
+      state2 = "open";
+      // close #1
+      content1.textContent = "";
+      state1 = "closed";
+      hoverBox1.className = "hoverRightArrow";
+      arrow1.className = "fa fa-arrow-right arrowIcon fa-1x";
+      // close #3
+      content3.textContent = "";
+      state3 = "closed";
+      hoverBox3.className = "hoverRightArrow";
+      arrow3.className = "fa fa-arrow-right arrowIcon fa-1x";
   } else {
     content2.textContent = "";
-    storageState2 = "closed";
+    state2 = "closed";
     hoverBox2.className = "hoverRightArrow";
-    arrow2.className = "fa fa-arrow-right arrowIcon";
+    arrow2.className = "fa fa-arrow-right arrowIcon fa-1x";
   }
 }
 
-var storageState3 = "closed";
+var state3 = "closed";
 var content3 = document.querySelector("#content3");
 var event3 = document.querySelector("#hoverBox3");
 var arrow3 = document.querySelector("#arrow3");
@@ -55,19 +75,32 @@ var hoverBox3 = document.querySelector("#hoverBox3");
 event3.addEventListener('click', toggle3);
 
 function toggle3() {
-  if (storageState3 === "closed") {
-      content3.textContent = text1;
-      arrow3.className = "fa fa-arrow-down arrowIcon";
-      hoverBox3.className = "hoverDownArrow";
-      storageState3 = "open";
+  if (state3 === "closed") {
+    content3.textContent = text1;
+    arrow3.className = "fa fa-arrow-down arrowIcon fa-1x";
+    hoverBox3.className = "hoverDownArrow";
+    state3 = "open";
+    // close #1
+    content1.textContent = "";
+    state1 = "closed";
+    hoverBox1.className = "hoverRightArrow";
+    arrow1.className = "fa fa-arrow-right arrowIcon fa-1x";
+    // close #2
+    content2.textContent = "";
+    state2 = "closed";
+    hoverBox2.className = "hoverRightArrow";
+    arrow2.className = "fa fa-arrow-right arrowIcon fa-1x";
   } else {
     content3.textContent = "";
-    storageState3 = "closed";
+    state3 = "closed";
     hoverBox3.className = "hoverRightArrow";
-    arrow3.className = "fa fa-arrow-right arrowIcon";
+    arrow3.className = "fa fa-arrow-right arrowIcon fa-1x";
   }
 }
-
+// *******************************************************
+//   Below is a few snippets from several different
+//   scapped attemps wanted to save.
+// *******************************************************
 // var storageState2 = "closed";
 //
 // var content2 = document.querySelector("#content2");
