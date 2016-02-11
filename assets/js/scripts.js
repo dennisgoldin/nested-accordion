8e4e84a0d03c1b7788671e8ed4d7bfb176434aeb
Dennis Goldin <dennis.goldin@gmail.com>
2016-02-10 23:49:52 -0500

    Normal mode works.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,8 @@
 var text1 = "Jelly sweet roll drage gummies. Jelly-o cotton candy gingerbread. Dessert tart bear claw gummi bears I love oat cake cotton candy sesame snaps carrot cake. Croissant marshmallow tiramisu icing chocolate cake. I love pie icing. Sweet I love sweet roll cheesecake dessert. Tiramisu gingerbread I love. Muffin chocolate cake powder cheesecake oat cake tootsie roll candy. Brownie toffee powder tiramisu. Wafer I love jujubes croissant. Chocolate cake candy jujubes tootsie roll bonbon toffee I love.";
-var text2 = "Cotton candy chocolate cake macaroon applicake I love. Tart soufflé I love powder. I love tiramisu lollipop pie. Pudding gummi bears sweet. Candy chocolate I love jujubes pie donut. Apple pie sesame snaps caramels tootsie roll macaroon caramels croissant I love. Sweet roll chocolate lemon drops. I love I love muffin gummi bears I love cotton candy.";
-var text3 = "Lemon drops ice cream I love danish soufflé cake jelly cupcake. Bear claw I love I love tart I love liquorice bear claw. Halvah chocolate gummi bears jelly-o jelly-o lollipop lemon drops liquorice. Candy canes tiramisu candy faworki apple pie. Chocolate jujubes sesame snaps ice cream cupcake. Halvah topping chupa chups biscuit. I love I love chupa chups tootsie roll marshmallow tart gummi bears chocolate.";
+var text2 = "Cotton candy chocolate cake macaroon applicake I love. Tart I love powder. I love tiramisu lollipop pie. Pudding gummi bears sweet. Candy chocolate I love jujubes pie donut. Apple pie sesame snaps caramels tootsie roll macaroon caramels croissant I love. Sweet roll chocolate lemon drops. I love I love muffin gummi bears I love cotton candy.";
+var text3 = "Lemon drops ice cream I love danish cake jelly cupcake. Bear claw I love I love tart I love liquorice bear claw. Halvah chocolate gummi bears jelly-o jelly-o lollipop lemon drops liquorice. Candy canes tiramisu candy faworki apple pie. Chocolate jujubes sesame snaps ice cream cupcake. Halvah topping chupa chups biscuit. I love I love chupa chups tootsie roll marshmallow tart gummi bears chocolate.";
 
 var storageState1 = "closed";
-
 var content1 = document.querySelector("#content1");
 var event1 = document.querySelector("#hoverBox1");
 var arrow1 = document.querySelector("#arrow1");
@@ -26,38 +25,82 @@ function toggle1() {
 }
 
 var storageState2 = "closed";
-
 var content2 = document.querySelector("#content2");
-var event2 = document.querySelector("#title2");
+var event2 = document.querySelector("#hoverBox2");
+var arrow2 = document.querySelector("#arrow2");
+var hoverBox2 = document.querySelector("#hoverBox2");
 
 event2.addEventListener('click', toggle2);
 
 function toggle2() {
   if (storageState2 === "closed") {
-      content2.textContent = text2;
+      content2.textContent = text1;
+      arrow2.className = "fa fa-arrow-down arrowIcon";
+      hoverBox2.className = "hoverDownArrow";
       storageState2 = "open";
   } else {
     content2.textContent = "";
     storageState2 = "closed";
+    hoverBox2.className = "hoverRightArrow";
+    arrow2.className = "fa fa-arrow-right arrowIcon";
   }
 }
 
 var storageState3 = "closed";
-
 var content3 = document.querySelector("#content3");
-var event3 = document.querySelector("#title3");
+var event3 = document.querySelector("#hoverBox3");
+var arrow3 = document.querySelector("#arrow3");
+var hoverBox3 = document.querySelector("#hoverBox3");
 
 event3.addEventListener('click', toggle3);
 
 function toggle3() {
   if (storageState3 === "closed") {
-      content3.textContent = text3;
+      content3.textContent = text1;
+      arrow3.className = "fa fa-arrow-down arrowIcon";
+      hoverBox3.className = "hoverDownArrow";
       storageState3 = "open";
   } else {
     content3.textContent = "";
     storageState3 = "closed";
+    hoverBox3.className = "hoverRightArrow";
+    arrow3.className = "fa fa-arrow-right arrowIcon";
   }
 }
+
+// var storageState2 = "closed";
+//
+// var content2 = document.querySelector("#content2");
+// var event2 = document.querySelector("#title2");
+//
+// event2.addEventListener('click', toggle2);
+//
+// function toggle2() {
+//   if (storageState2 === "closed") {
+//       content2.textContent = text2;
+//       storageState2 = "open";
+//   } else {
+//     content2.textContent = "";
+//     storageState2 = "closed";
+//   }
+// }
+
+// var storageState3 = "closed";
+//
+// var content3 = document.querySelector("#content3");
+// var event3 = document.querySelector("#title3");
+//
+// event3.addEventListener('click', toggle3);
+//
+// function toggle3() {
+//   if (storageState3 === "closed") {
+//       content3.textContent = text3;
+//       storageState3 = "open";
+//   } else {
+//     content3.textContent = "";
+//     storageState3 = "closed";
+//   }
+// }
 
 // var title = document.querySelector(".entryTitle");
 // var content = document.querySelector(".entryContent");
